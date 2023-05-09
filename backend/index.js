d80aa9ca9f70523db7f9992a79d6c801a686bfb5
Andrey <antonov.andrey.10101@gmail.com>
2023-05-09 08:49:50 +0300

    feat: add error processing

--- backend/index.js
+++ backend/index.js
@@ -22,26 +22,46 @@ const app = express();
 app.use(express.json());
 
 app.post("/auth/register", registerValidation, async (req, res) => {
-    const errors = validationResult(req);
+    try {
+        const errors = validationResult(req);
 
-    const password = req.body.password;
-    const salt = await bcrypt.genSalt(10);
-    const passwordHash = await bcrypt.hash(password, salt);
+        if (!errors.isEmpty()) {
+            return res.status(400).json(errors.array());
+        }
 
-    const doc = new UserModel({
-        email: req.body.email,
-        fullName: req.body.fullName,
-        avatarUrl: req.body.avatarUrl,
-        passwordHash,
-    });
+        const password = req.body.password;
+        const salt = await bcrypt.genSalt(10);
+        const hash = await bcrypt.hash(password, salt);
 
-    const user = await doc.save();
+        const doc = new UserModel({
+            email: req.body.email,
+            fullName: req.body.fullName,
+            avatarUrl: req.body.avatarUrl,
+            passwordHash: hash,
+        });
 
-    if (!errors.isEmpty()) {
-        return res.status(400).json(errors.array());
+        const user = await doc.save();
+
+        const token = jwt.sign(
+            {
+                _id: user._id,
+            },
+            "secret123",
+            {
+                expiresIn: "30d",
+            }
+        );
+
+        const { passwordHash, ...userData } = user._doc;
+
+        res.json({ ...userData, token });
+    } catch (error) {
+        console.log(error);
+
+        res.status(500).json({
+            message: "Не удалось зарегистрироваться",
+        });
     }
-
-    res.json(user);
 });
 
 app.listen(4444, (err) => {
