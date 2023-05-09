84a0d0ad7e80f6929592ce838089e93d0f03e030
Andrey <antonov.andrey.10101@gmail.com>
2023-05-09 08:41:43 +0300

    feat: add basic project structure

---
+++ backend/index.js
@@ -0,0 +1,24 @@
+import express from "express";
+
+const app = express();
+
+app.use(express.json())
+
+app.get("/", (req, res) => {
+    res.send("Hello world!");
+});
+
+app.post("/auth/login", (req, res) => {
+    console.log(req.body);
+    res.json({
+        success: true,
+    });
+});
+
+app.listen(4444, (err) => {
+    if (err) {
+        return console.log(err);
+    }
+
+    console.log("Server Ok");
+});
