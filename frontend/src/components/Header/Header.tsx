08b7a48201c247964bd090e2ea02bd9fd8f736a8
Andrey <antonov.andrey.10101@gmail.com>
2023-05-09 12:48:11 +0300

    feat: add Header component

---
+++ frontend/src/components/Header/Header.tsx
@@ -0,0 +1,56 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+import "./Header.scss";
+import { useDispatch, useSelector } from "react-redux";
+import { logout, selectIsAuth } from "../../redux/slices/auth";
+import { Button, Flex } from "@chakra-ui/react";
+
+export const Header = () => {
+  const dispatch = useDispatch();
+  const isAuth = useSelector(selectIsAuth);
+
+  const onClickLogout = () => {
+    if (window.confirm("Вы действительно хотите выйти?")) {
+      dispatch(logout());
+      window.localStorage.removeItem("token");
+    }
+  };
+
+  return (
+    <div className='header'>
+      <Flex maxWidth="lg">
+        <div className='header__inner'>
+          <Link className='header__logo' to="/">
+            <div>VK</div>
+          </Link>
+          <div className='header__buttons'>
+            {isAuth ? (
+              <>
+                <Link to="/add-post">
+                  <Button variant="contained">Написать статью</Button>
+                </Link>
+                <Button
+                  onClick={onClickLogout}
+                  variant="contained"
+                  color="error"
+                >
+                  Выйти
+                </Button>
+              </>
+            ) : (
+              <>
+                <Link to="/login">
+                  <Button variant="outlined">Войти</Button>
+                </Link>
+                <Link to="/register">
+                  <Button variant="contained">Создать аккаунт</Button>
+                </Link>
+              </>
+            )}
+          </div>
+        </div>
+      </Flex>
+    </div>
+  );
+};
--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,12 +1,17 @@
 import { Flex } from "@chakra-ui/react";
 import { Post } from "../components/Post/Post";
+import { Header } from "../components/Header/Header";
 
 export const Home = () => {
   return (
-    <Flex alignItems="center" justifyContent="center" p="20px">
-      <Flex>
-        <Post isFullPost={false} />
-      </Flex>
+    <Flex
+      alignItems="center"
+      justifyContent="center"
+      p="20px"
+      flexDirection="column"
+    >
+      <Header />
+      <Post isFullPost={false} />
     </Flex>
   );
 };
