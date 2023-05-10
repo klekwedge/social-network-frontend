158d9487cb433edf1beb1cf5f98869b35f4cbaff
Andrey <antonov.andrey.10101@gmail.com>
2023-05-10 20:50:09 +0300

    feat: left only the frontend in the project

--- src/axios.ts
+++ src/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://localhost:4444",
+  baseURL: import.meta.env.VITE_API || "http://localhost:4444",
 });
 
 instance.interceptors.request.use((config) => {
